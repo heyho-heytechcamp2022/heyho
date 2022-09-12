61bf961fb99ef771e245d8e74308c54337aae414
HosokawaR <thin.river.jp@gmail.com>
2022-09-12 14:50:38 +0900

    style: format

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+});
+
+createApp(App).use(router).mount("#app");

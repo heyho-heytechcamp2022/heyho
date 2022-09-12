15fd8f23c523ce58200cb6929a36883f67050267
HosokawaR <thin.river.jp@gmail.com>
2022-09-12 14:55:20 +0900

    feat: add fetching from ec site and saving to firestore

---
+++ firebase/functions/src/ec.ts
@@ -0,0 +1,117 @@
+import { DocumentReference } from "firebase-admin/firestore";
+import { db } from "./init";
+import cryptoRandomString from "crypto-random-string";
+
+type Item = { id: string; name: string };
+type Order = {
+  id: string;
+  customerId: string;
+  items: { id: string; quantity: number }[];
+};
+type Customer = {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+};
+
+const generateIam = () => cryptoRandomString({ length: 32, type: "url-safe" });
+
+const saveToFirestore = async (data: {
+  items: Item[];
+  orders: Order[];
+  customers: Customer[];
+}) => {
+  // TODO: use transactions
+
+  const items = data.items;
+  const orders = data.orders;
+  const customers = data.customers;
+
+  const itemRefMap = new Map<Item["id"], DocumentReference>();
+
+  items.forEach(async (item) => {
+    const newItemRef = await db.collection("items").add(item);
+    itemRefMap.set(item.id, newItemRef);
+  });
+
+  const customerRefMap = new Map<Customer["id"], DocumentReference>();
+
+  customers.forEach(async (customer) => {
+    const newCustomerRef = await db.collection("customers").add(customer);
+    customerRefMap.set(customer.id, newCustomerRef);
+  });
+
+  orders.forEach(async (order) => {
+    await db.collection("orders").add({
+      ...order,
+      items: order.items.map((item) => {
+        ({
+          ...item,
+          itemRef: itemRefMap.get(item.id),
+        });
+      }),
+      customerRef: customerRefMap.get(order.customerId),
+      status: "unadjusted",
+      iam: generateIam(),
+    });
+  });
+};
+
+export const fetchFromEc = async (
+  type: "stoers" | "base" | "shopify",
+  shopId: string,
+  apiKey: string
+) => {
+  if (type === "stoers") {
+  } else if (type === "base") {
+    return;
+  } else if (type === "shopify") {
+    return;
+  }
+};
+
+const dummyItems: Item[] = [
+  { id: "1", name: "item1" },
+  { id: "2", name: "item2" },
+  { id: "3", name: "item3" },
+];
+
+const dummyOrders: Order[] = [
+  { id: "1", customerId: "1", items: [{ id: "1", quantity: 1 }] },
+  {
+    id: "2",
+    customerId: "2",
+    items: [
+      { id: "1", quantity: 1 },
+      { id: "2", quantity: 2 },
+    ],
+  },
+];
+
+const dummyCustomers: Customer[] = [
+  {
+    id: "1",
+    name: "customer1",
+    email: "customer1@example.com",
+    phone: "000-0000-0000",
+    address: "address1",
+  },
+  {
+    id: "2",
+    name: "customer2",
+    email: "customer2@example.com",
+    phone: "001-0001-0001",
+    address: "address2",
+  },
+];
+
+const fetchFromStoers = async (shopId: string, apiKey: string) => {
+  // TODO: implement
+  return {
+    items: dummyItems,
+    orders: dummyOrders,
+    customers: dummyCustomers,
+  };
+};
---
+++ firebase/functions/src/init.ts
@@ -0,0 +1,6 @@
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+const app = initializeApp();
+
+export const db = getFirestore(app);

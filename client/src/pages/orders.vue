72eae04407a35f3f4eb73d87b0e7b495f3061617
HosokawaR <thin.river.jp@gmail.com>
2022-09-12 15:03:49 +0900

    feat: add order list page

--- client/src/pages/orders.vue
+++ client/src/pages/orders.vue
@@ -8,21 +8,71 @@ import {
   getDoc,
   updateDoc,
   query,
+  DocumentReference,
 } from "firebase/firestore";
 import { db, getUser } from "~/firebase";
 
 const route = useRoute();
 const { userId } = await getUser();
 
+// TODO: route gurde ができたら delete
+if (!userId) {
+  throw new Error("userId is not found");
+}
+
 const eventId = String(route.params.eventId);
 
 const q = query(collection(db, `users/${userId}/events/${eventId}/items`));
 const querySnapshot = await getDocs(q);
-const items = ref(
-  querySnapshot.docs.map((doc) => ({
-    docId: doc.id,
-    ...doc.data(),
-  }))
+const items = querySnapshot.docs.map((doc) => ({
+  docId: doc.id,
+  ...(doc.data() as { id: string; name: string; eventRef: DocumentReference }),
+}));
+
+const q2 = query(collection(db, `users/${userId}/events/${eventId}/orders`));
+const querySnapshot2 = await getDocs(q2);
+const orders = querySnapshot2.docs.map((doc) => ({
+  docId: doc.id,
+  ...(doc.data() as {
+    id: string;
+    name: string;
+    customerId: string;
+    customerRef: DocumentReference;
+    iam: string;
+    items: { id: string; itemRef: DocumentReference; quantity: number }[];
+    status: string;
+  }),
+}));
+
+console.log(items);
+
+const handlingItems = items.filter(
+  (item) =>
+    item.eventRef?.id === doc(db, `users/${userId}/events/${eventId}`).id
+);
+
+const handlingOrders = orders
+  .map((order) => {
+    return {
+      ...order,
+      items: order.items.filter((item) =>
+        handlingItems.some(
+          (handlingItem) => handlingItem.docId === item.itemRef.id
+        )
+      ),
+    };
+  })
+  .filter((order) => order.items.length > 0);
+
+const handlingOrdersAndCustomers = await Promise.all(
+  handlingOrders.map(async (order) => {
+    console.log(order);
+    return {
+      order,
+      // TODO: 並列化
+      customer: await getDoc(order.customerRef).then((doc) => doc.data()),
+    };
+  })
 );
 
 // TODO: リロードしなくても反映されるように
@@ -49,6 +99,17 @@ const updateItemEvent = async (id: string, eventId: string) => {
         選択
       </button>
       <button v-else @click="updateItemEvent(item.docId, '')">解除</button>
+    </div>
+  </div>
+  <h3>注文一覧</h3>
+
+  <div
+    v-for="orderAndCustomer in handlingOrdersAndCustomers"
+    :key="orderAndCustomer.order.id"
+  >
+    {{ orderAndCustomer.customer?.name }} {{ orderAndCustomer.customer?.email }}
+    <div v-for="item in orderAndCustomer.order.items" :key="item.id">
+      {{ item.itemRef.id }} {{ item.quantity }}
     </div>
   </div>
 </template>

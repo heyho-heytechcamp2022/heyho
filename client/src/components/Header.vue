8b3eb55737f9d3ceadae91ac1ac47ab8063e0a47
yu1hpa <yu.1hpa@gmail.com>
2022-09-14 15:07:16 +0900

    feat: Add header component

---
+++ client/src/components/Header.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup></script>
+<template>
+  <header>
+    <div class="header_container">
+      <div class="header">
+        <img src="../assets/heyho.svg" alt="heyho_logo" />
+        <slot></slot>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header_container {
+  max-width: 1200px;
+  padding: 0 25px;
+  margin: auto;
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+}
+</style>

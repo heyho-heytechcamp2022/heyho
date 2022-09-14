54cec4c8f49598835ab052a6c8c10469fabd5987
yu1hpa <yu.1hpa@gmail.com>
2022-09-14 16:31:22 +0900

    style: Styling

--- client/src/components/Input.vue
+++ client/src/components/Input.vue
@@ -1,35 +1,36 @@
 <script lang="ts" setup>
-    type Props = {
-        modelValue: string | number,
-        type: "text" | "number",
-    };
-    const props = defineProps<Props>();
-    const emit = defineEmits(["update:modelValue", "enter-text-field"]);
-    const handleInput = (e: Event) => {
-        if (!(e.target instanceof HTMLInputElement)) {
-            return;
-        }
-        emit("update:modelValue", e.target.value);
-    }; 
-    const handleEnter = (e: KeyboardEvent) => {
-        emit("enter-text-field", e);
-    };
+type Props = {
+  modelValue: string | number;
+  type: "text" | "number";
+};
+const props = defineProps<Props>();
+const emit = defineEmits(["update:modelValue", "enter-text-field"]);
+const handleInput = (e: Event) => {
+  if (!(e.target instanceof HTMLInputElement)) {
+    return;
+  }
+  emit("update:modelValue", e.target.value);
+};
+const handleEnter = (e: KeyboardEvent) => {
+  emit("enter-text-field", e);
+};
 </script>
 <template>
-    <input :type="type"
+  <input
+    :type="type"
     :value="modelValue"
     :class="{
-        'text-field': true,
+      'text-field': true,
     }"
     @input="handleInput"
     @keydown.enter="handleEnter"
-    />
- </template>
+  />
+</template>
 
 <style lang="scss" scoped>
 .text-field {
-    padding: 7px;
-    border: 2px solid #ccc;
-    border-radius: 3px;
+  padding: 7px;
+  border: 2px solid #ccc;
+  border-radius: 3px;
 }
 </style>

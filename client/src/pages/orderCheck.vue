5d0a8637855d4e41f385cf3cccc026b2d4983528
yu1hpa <yu.1hpa@gmail.com>
2022-09-15 03:46:50 +0900

    [wip]feat: Add status change processing

--- client/src/pages/orderCheck.vue
+++ client/src/pages/orderCheck.vue
@@ -1,22 +1,52 @@
 <script setup lang="ts">
-import { ref, reactive, computed } from "vue";
+import { httpsCallable } from "@firebase/functions";
+import { CommonFunctions } from "@common";
+import { ref, reactive } from "vue";
 import QRReader from "~/components/QRReader.vue";
+import * as t from "io-ts";
+import { Functions } from "~/types";
+import { functions } from "~/firebase";
 
 interface IState {
-  /** QRコードデータ */
-  qrCode: string;
+  iamFromQr: string;
 }
 
 const state = reactive<IState>({
-  qrCode: "",
+  iamFromQr: "",
 });
 
-const isUrl = computed(() => {
-  return /^https?:\/\//.test(state.qrCode);
-});
+const onScan = async (code: string) => {
+  console.log("onScannn");
+  state.iamFromQr = code;
+  const _result = await httpsCallable<
+    t.TypeOf<typeof CommonFunctions.FindOrderByIam.In>,
+    t.TypeOf<typeof Functions.FindOrderByIam.Out>
+  >(
+    functions,
+    "findOrderByIam"
+  )({ iam: String(state.iamFromQr) }).then((res) => res.data);
 
-const onScan = (code: string) => {
-  state.qrCode = code;
+  console.log("done iam");
+  const result = _result.body;
+
+  const orderStatus = ref(result.order.data.status);
+  const res = (await httpsCallable<
+    t.TypeOf<typeof CommonFunctions.UpdateOrderStatus.In>,
+    t.TypeOf<typeof CommonFunctions.UpdateOrderStatus.Out>
+  >(
+    functions,
+    "updateOrderStatus"
+  )({
+    ownerId: result.owner.id,
+    eventId: result.event.id,
+    orderId: result.order.id,
+  }).then((res) => res.data)) as { status: string };
+
+  console.log("presuccess");
+  if (res.status === "ok") {
+    orderStatus.value = "completed";
+    location.reload();
+  }
 };
 
 const isEnable = ref(false);
@@ -29,10 +59,7 @@ const isEnable = ref(false);
       <QRReader @scan="onScan" v-if="isEnable"></QRReader>
       <div class="info">
         <div>QRデータ:</div>
-        <div v-if="isUrl">
-          <a :href="state.qrCode" target="_blank">{{ state.qrCode }}</a>
-        </div>
-        <div>{{ state.qrCode }}</div>
+        <div>{{ state.iamFromQr }}</div>
       </div>
     </div>
   </div>

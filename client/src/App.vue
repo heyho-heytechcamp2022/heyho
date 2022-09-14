b12044a03490f65ab914f084ee847f4210c5db3d
yu1hpa <yu.1hpa@gmail.com>
2022-09-14 10:45:43 +0900

    feat(signout): Add SignOut button and processing

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { auth, getUser } from "~/firebase";
+import { signOut, Auth } from "firebase/auth";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+const handleClick = () => {
+  signOut(auth).then(() => {
+    router.push("/login");
+    console.log("success");
+  });
+};
+let isLogin = ref(false);
+getUser().then(({ user, userId }) => {
+  isLogin.value = Boolean(user);
+});
+</script>
 
 <template>
   <Suspense>
     <template #default>
       <Layout>
+        <button v-if="isLogin" @click="handleClick()">SignOut</button>
         <router-view />
       </Layout>
     </template>
